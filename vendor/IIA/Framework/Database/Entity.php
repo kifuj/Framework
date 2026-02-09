5521c0a3b84e07e0e6df4f2ca8c36352788025f4
Matteo <lamarematteo53@gmail.com>
2026-02-09 23:57:27 +0100

    second commit

--- vendor/IIA/Framework/Database/Entity.php
+++ vendor/IIA/Framework/Database/Entity.php
@@ -7,6 +7,7 @@ class Entity
     protected Database $database;
     protected string $table;
     protected int $id;
+    protected static $tableName = null;
 
     public function __construct(Database $database)
     {
@@ -18,14 +19,42 @@ class Entity
         return $this->id;
     }
 
-    public function getAll(): array
+    public function getAll( string $condition = none , string $order = none , int $lim = none): array
     {
-        return $this->database->query("SELECT * FROM " . $this->table, static::class);
+        return $this->database->query("SELECT * FROM " . $this->table . "WHERE" . $condition . "ORDER BY" . $order . "LIMIT" . $lim , static::class);
     }
 
+        public function getOne(int $ids): array
+    {
+        return $this->database->query("SELECT * FROM " . $this->table . "WHERE" . $ids, static::class);
+    }
     public function __get(string $key)
     {
         $method = 'get' . ucfirst($key);
         return $this->$method();
+    }
+
+    public static function getTableName(): string
+    {
+        if (static::$tableName !== null) {
+            return static::$tableName;
+        }
+        
+        $className = (new \ReflectionClass(static::class))->getShortName();
+        
+        return self::toSnakeCase($className);
+    }
+    
+
+    public static function setTableName(string $name): void
+    {
+        static::$tableName = $name;
+    }
+
+    protected static function toSnakeCase(string $string): string
+    {
+        $snake = preg_replace('/(?<!^)[A-Z]/', '_$0', $string);
+        
+        return strtolower($snake);
     }
 }
